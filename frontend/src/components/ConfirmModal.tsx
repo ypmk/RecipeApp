7448ecccd0027641d3cbdd7be3cc6a2e09125417
ypmk <kopulovaksusha123@gmail.com>
2025-04-13 16:54:19 +0300

    Изменен стиль всплывающего окна подтверждения удаления (убран наклон шрифта)

--- frontend/src/components/ConfirmModal.tsx
+++ frontend/src/components/ConfirmModal.tsx
@@ -13,10 +13,13 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({ isOpen, title, message, onC
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40">
-            <div className="bg-white rounded-xl shadow-lg p-6 w-[90%] max-w-md">
-                {title && <h2 className="text-xl font-bold mb-4">{title}</h2>}
-                <p className="text-gray-800 mb-6">{message}</p>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40 font-[sans-serif]">
+            <div className="bg-white rounded-xl shadow-lg p-6 w-[90%] max-w-md font-normal">
+
+            {title && <h2 className="text-xl font-bold not-italic mb-4">{title}</h2>}
+                <p className="text-gray-800 font-normal mb-6">{message}</p>
+
+
                 <div className="flex justify-end gap-4">
                     <button
                         className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300 text-gray-800"
