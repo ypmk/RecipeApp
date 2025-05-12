1d0723a4e10fab34ec487f9427196caacac77a5a
ypmk <kopulovaksusha123@gmail.com>
2025-05-12 13:39:31 +0300

    Добавлена адаптивность для модального окна подтверждения

--- frontend/src/components/ConfirmModal.tsx
+++ frontend/src/components/ConfirmModal.tsx
@@ -1,5 +1,4 @@
-// components/ConfirmModal.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type ConfirmModalProps = {
     isOpen: boolean;
@@ -12,9 +11,22 @@ type ConfirmModalProps = {
 const ConfirmModal: React.FC<ConfirmModalProps> = ({ isOpen, title, message, onCancel, onConfirm }) => {
     if (!isOpen) return null;
 
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, [isOpen]);
+
+
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40 font-[sans-serif]">
-            <div className="bg-white rounded-xl shadow-lg p-6 w-[90%] max-w-md font-normal">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40 font-[sans-serif] px-4">
+            <div className="bg-white rounded-xl shadow-lg p-6 w-full max-w-sm box-border overflow-hidden">
 
             {title && <h2 className="text-xl font-bold not-italic mb-4 text-center">{title}</h2>}
                 <p className="text-gray-800 font-normal mb-6 text-center">{message}</p>
