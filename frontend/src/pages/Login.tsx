604fb9826e511cc1e21a8db23061a6a71e3c595f
ypmk <kopulovaksusha123@gmail.com>
2025-05-16 14:43:05 +0300

    Для сервера

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -14,7 +14,7 @@ const Login: React.FC = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:3000/api/login', {
+            const response = await fetch('http://api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -10,7 +10,7 @@ const Register: React.FC = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:3000/api/register', {
+            const response = await fetch('http://api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
