e7520d572008dac366b96bbeeaa9aa3c9d6fc7d9
ypmk <kopulovaksusha123@gmail.com>
2025-05-16 14:44:58 +0300

    Для сервера

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -10,7 +10,7 @@ const Register: React.FC = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://api/register', {
+            const response = await fetch('/api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
